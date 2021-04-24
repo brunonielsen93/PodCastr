af86f40ea1dcd7c035516687abad90c6820c7a51
Bruno Nielsen <programandowithjava@gmail.com>
2021-04-24 15:18:48 -0300

    Separando player unico

---
+++ src/contexts/PlayerContext.tsx
@@ -0,0 +1,93 @@
+import { createContext, useState, ReactNode, useContext } from 'react';
+
+interface Episode {
+  title: string;
+  members: string;
+  thumbnail: string;
+  duration: string;
+  url: string;
+};
+
+interface PlayerContextData {
+  episodeList: Episode[];
+  currentEpisodeIndex: number;
+  isPlaying: boolean;
+  play: (episode: Episode) => void;
+  playList: (list: Episode[], index: number) => void;
+  setPlayingState: (state: boolean) => void;
+  togglePlay: () => void;
+  playNext: () => void;
+  playPrevious: () => void;
+  hasNext: boolean;
+  hasPrevious: boolean;
+};
+
+export const PlayerContext = createContext({} as PlayerContextData);
+
+type PlayerContextProviderProps = {
+  children: ReactNode;
+}
+
+export function PlayerContextProvider({ children }: PlayerContextProviderProps) {
+  const [episodeList, setEpisodeList] = useState([]);
+  const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  function play(episode: Episode) {
+    setEpisodeList([episode]);
+    setCurrentEpisodeIndex(0);
+    setIsPlaying(true);
+  }
+
+  function playList(list: Episode[], index: number) {
+    setEpisodeList(list);
+    setCurrentEpisodeIndex(index);
+    setIsPlaying(true);
+  }
+
+  function togglePlay() {
+    setIsPlaying(!isPlaying);
+  }
+
+  function setPlayingState(state: boolean) {
+    setIsPlaying(state);
+  }
+
+  const hasPrevious = currentEpisodeIndex > 0;
+  const hasNext = (currentEpisodeIndex + 1) < episodeList.length
+
+  function playNext() {
+    if (hasNext) {
+      setCurrentEpisodeIndex(currentEpisodeIndex + 1);
+    }
+  }
+
+  function playPrevious() {
+    if (hasPrevious) {
+      setCurrentEpisodeIndex(currentEpisodeIndex  - 1);
+    }
+  }
+
+  return (
+    <PlayerContext.Provider 
+    value={{ 
+      episodeList, 
+      currentEpisodeIndex, 
+      play,
+      playList,
+      playNext,
+      playPrevious, 
+      isPlaying, 
+      togglePlay, 
+      setPlayingState,
+      hasNext,
+      hasPrevious, 
+      }}>
+      { children }
+    </PlayerContext.Provider>
+  )
+}
+
+export const usePlayer = () => {
+  return useContext(PlayerContext);
+}
--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -10,6 +10,7 @@ import { api } from '../../services/api';
 import { convertDurationToTimeString } from '../../Utils/convertDurationToTimeString';
 
 import styles from './episode.module.scss';
+import {  usePlayer } from '../../contexts/PlayerContext';
 
 interface Episode {
     id: string;
@@ -28,6 +29,8 @@ episode: Episode;
 }
 
 export default function Episode({ episode }: EpisodeProps) {
+  const { play } = usePlayer();
+
   return (
     <div className={styles.episode}>
       <div className={styles.thumbnailContainer}>
@@ -42,7 +45,7 @@ export default function Episode({ episode }: EpisodeProps) {
         src={episode.thumbnail}
         objectFit="cover"
         />
-        <button type="button">
+        <button type="button" onClick={() => play(episode)}>
           <img src="/play.svg" alt="Tocar episódio" />
         </button>
       </div>
